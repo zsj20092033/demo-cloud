c0fffa16c8b3d468164364687af6aa846c67446b
clock <clock@careerintlinc.com>
2019-01-24 13:33:03 +0800

    init

---
+++ spring-cloud-config/src/main/java/demo/cloud/config/ConfigServerApplication.java
@@ -0,0 +1,19 @@
+package demo.cloud.config;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.config.server.EnableConfigServer;
+
+/**
+ * @author clock
+ * @date 2019/1/21 下午5:23
+ */
+@SpringBootApplication
+@EnableConfigServer
+public class ConfigServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ConfigServerApplication.class, args);
+    }
+
+}
